47fcc38d61590eaa03eab4dd88914e8c94c4be44
Refone <hustliuruifeng@gmail.com>
2017-12-01 16:03:30 +0800

    first commit

---
+++ cdf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import sys
+
+
+def cdf_plot(data, name, number):
+    """
+    data: 一组数据
+    name: 在legend上显示的名称
+    number: 数据最大最小值之间划分多少段
+    """
+    ecdf = sm.distributions.ECDF(data)
+    x = np.linspace(min(data), max(data), number)
+    y = ecdf(x)
+
+    #plt.step(x, y, label=name)
+    plt.plot(x, y, label=name)
+
+
+def main():
+    for f in sys.argv[1:]:
+        data = open(f).readlines()
+        data = map(float, data)
+        cdf_plot(data, f, 100)
+
+    #plt.xscale('log')
+    plt.legend(bbox_to_anchor=(0.65, 0.3), loc=2, borderaxespad=0.)
+    plt.show() #显示CDF图
+
+if __name__ == '__main__':
+    main()
